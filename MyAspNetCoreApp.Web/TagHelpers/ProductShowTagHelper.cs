dcd1609116417d23e9a9c6130263da236774e529
durmazoguzhan <durmazoguzhan@yahoo.com>
2023-08-23 19:14:49 +0300

    added ProductShowTagHelper.cs

---
+++ MyAspNetCoreApp.Web/TagHelpers/ProductShowTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using MyAspNetCoreApp.Web.ViewModels;
+
+namespace MyAspNetCoreApp.Web.TagHelpers
+{
+    public class ProductShowTagHelper : TagHelper
+    {
+        public ProductViewModel Product { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.SetHtmlContent($"" +
+                $"<ul class=\"list-group mb-3\">" +
+                $"<li class=\"list-group-item\">{Product.Name}</li>" +
+                $"<li class=\"list-group-item\">{Product.Color}</li>" +
+                $"<li class=\"list-group-item\">{Product.Price}</li>" +
+                $"<li class=\"list-group-item\">{Product.Stock}</li>" +
+                $"</ul>");
+        }
+    }
+}
