fcbe2500a5e8c9a067ea9856204011700f020a55
durmazoguzhan <durmazoguzhan@yahoo.com>
2023-08-23 21:18:13 +0300

    added ImageThumbnailTagHelper.cs

---
+++ MyAspNetCoreApp.Web/TagHelpers/ImageThumbnailTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MyAspNetCoreApp.Web.TagHelpers
+{
+    public class ImageThumbnailTagHelper : TagHelper
+    {
+        public string ImageSrc { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string fileName = ImageSrc.Split(".")[0];
+            string fileExtension = Path.GetExtension(ImageSrc); // .png or .jpg or ...
+
+            output.TagName = "img"; // <img></img>
+            output.Attributes.SetAttribute("src", $"{fileName}-128x128{fileExtension}");
+        }
+    }
+}
